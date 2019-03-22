006fffa3745a9a7731d0cb0d8d0d239a9a2bbe70
Rui Pedro Moutinho Moreira Alves <up201606746@fe.up.pt>
2019-03-22 13:12:11 +0000

    Finished database implementation

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS IssueNotification;
+DROP TABLE IF EXISTS Issue;
 DROP TABLE IF EXISTS EventTag;
 DROP TABLE IF EXISTS Favorite;
 DROP TABLE IF EXISTS Organizing;
@@ -11,9 +13,7 @@ DROP TABLE IF EXISTS Tag;
 DROP TABLE IF EXISTS EventNotification;
 DROP TABLE IF EXISTS Event;
 DROP TABLE IF EXISTS EventCategory;
-DROP TABLE IF EXISTS IssueNotification;
 DROP TABLE IF EXISTS Notification;
-DROP TABLE IF EXISTS Issue;
 DROP TABLE IF EXISTS Person;
 DROP TYPE IF EXISTS TICKET_PAYMENT_TYPE;
 DROP TYPE IF EXISTS EVENT_NOTIFICATION_TYPE;
@@ -42,35 +42,12 @@ CREATE TABLE Person (
 	is_admin BOOLEAN NOT NULL DEFAULT false
 );
 
--- R02
-CREATE TABLE Issue (
-	issue_id SERIAL PRIMARY KEY,
-	title TEXT NOT NULL,
-	content TEXT NOT NULL,
-	"timestamp" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-	is_solved BOOLEAN NOT NULL DEFAULT false,
-    creator_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
-    solver_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
-	referenced_person INTEGER REFERENCES Person(person_id) ON UPDATE CASCADE
-	-- referenced_event INTEGER REFERENCES Event(event_id) ON UPDATE CASCADE,
-	-- referenced_announcement INTEGER REFERENCES Announcement(announcement_id) ON UPDATE CASCADE,
-	-- referenced_post INTEGER REFERENCES Post(post_id) ON UPDATE CASCADE,
-	-- referenced_comment INTEGER REFERENCES Comment(comment_id) ON UPDATE CASCADE
-);
-
 -- R03
 CREATE TABLE Notification (
     notification_id SERIAL PRIMARY KEY,
 	is_dismissed BOOLEAN NOT NULL DEFAULT false,
 	"timestamp" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
     person_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE
-);
-
--- R04
-CREATE TABLE IssueNotification(
-    notification_id INTEGER PRIMARY KEY REFERENCES Notification(notification_id) ON UPDATE CASCADE,
-	content TEXT NOT NULL,
-    issue_id INTEGER NOT NULL REFERENCES Issue(issue_id) ON UPDATE CASCADE
 );
 
 -- R07
@@ -155,13 +132,31 @@ CREATE TABLE Comment (
 CREATE TABLE Rating (
     person_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
     post_id INTEGER NOT NULL REFERENCES Post(post_id) ON UPDATE CASCADE,
+
     PRIMARY KEY (person_id, post_id)
+);
+
+-- R14
+CREATE TABLE Ticket (
+    person_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
+    event_id INTEGER NOT NULL REFERENCES Event(event_id) ON UPDATE CASCADE,
+	"timestamp" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+	is_checked_in BOOLEAN NOT NULL DEFAULT false,
+	check_in_organizer_id INTEGER REFERENCES Person(person_id) ON UPDATE CASCADE,
+	nif INTEGER DEFAULT 999999999,
+	billing_name TEXT,
+	address TEXT,
+    event_voucher_id INTEGER REFERENCES EventVoucher(event_voucher_id) ON UPDATE CASCADE,
+	TYPE TICKET_PAYMENT_TYPE NOT NULL,
+
+    PRIMARY KEY (person_id, event_id)
 );
 
 -- R15
 CREATE TABLE Organizing (
     person_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
     event_id INTEGER NOT NULL REFERENCES Event(event_id) ON UPDATE CASCADE,
+
     PRIMARY KEY (person_id, event_id)
 );
 
@@ -169,6 +164,7 @@ CREATE TABLE Organizing (
 CREATE TABLE Favorite (
     person_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
     event_id INTEGER NOT NULL REFERENCES Event(event_id) ON UPDATE CASCADE,
+
     PRIMARY KEY (person_id, event_id)
 );
 
@@ -176,7 +172,31 @@ CREATE TABLE Favorite (
 CREATE TABLE EventTag (
     event_id INTEGER NOT NULL REFERENCES Event(event_id) ON UPDATE CASCADE,
     tag_id INTEGER NOT NULL REFERENCES Tag(tag_id) ON UPDATE CASCADE,
+
     PRIMARY KEY (event_id, tag_id)
+);
+
+-- R02
+CREATE TABLE Issue (
+	issue_id SERIAL PRIMARY KEY,
+	title TEXT NOT NULL,
+	content TEXT NOT NULL,
+	"timestamp" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+	is_solved BOOLEAN NOT NULL DEFAULT false,
+    creator_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
+    solver_id INTEGER NOT NULL REFERENCES Person(person_id) ON UPDATE CASCADE,
+	referenced_person INTEGER REFERENCES Person(person_id) ON UPDATE CASCADE,
+	referenced_event INTEGER REFERENCES Event(event_id) ON UPDATE CASCADE,
+	referenced_announcement INTEGER REFERENCES Announcement(announcement_id) ON UPDATE CASCADE,
+	referenced_post INTEGER REFERENCES Post(post_id) ON UPDATE CASCADE,
+	referenced_comment INTEGER REFERENCES Comment(comment_id) ON UPDATE CASCADE
+);
+
+-- R04
+CREATE TABLE IssueNotification(
+    notification_id INTEGER PRIMARY KEY REFERENCES Notification(notification_id) ON UPDATE CASCADE,
+	content TEXT NOT NULL,
+    issue_id INTEGER NOT NULL REFERENCES Issue(issue_id) ON UPDATE CASCADE
 );
 
 
