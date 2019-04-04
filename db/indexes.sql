6d87d0e9eada7181c0f038c90506a254be8d47d1
Henrique Melo Lima <up201606525@fe.up.pt>
2019-04-04 08:38:16 +0100

    Changed multiple column hash indexes to btrees, added index drops

--- db/indexes.sql
+++ db/indexes.sql
@@ -1,19 +1,34 @@
-CREATE INDEX voucher_code_index ON table USING hash (code, event_id);
 
-CREATE INDEX posts_index ON posts USING hash (event_id, is_announcement);
+DROP INDEX IF EXISTS posts_index;
+DROP INDEX IF EXISTS voucher_code_index;
+DROP INDEX IF EXISTS posts_timestamp_index;
+DROP INDEX IF EXISTS posts_rating_index;
+DROP INDEX IF EXISTS comments_index;
+DROP INDEX IF EXISTS comments_timestamp_index;
+DROP INDEX IF EXISTS tickets_index;
+DROP INDEX IF EXISTS issues_timestamp_index;
+DROP INDEX IF EXISTS issues_solved_index;
+DROP INDEX IF EXISTS notifications_index;
+DROP INDEX IF EXISTS notifications_timestamp_index;
+DROP INDEX IF EXISTS event_invite_notification_spam;
+
+
+CREATE INDEX voucher_code_index ON event_vouchers USING btree (code, event_id);
+
+CREATE INDEX posts_index ON posts USING btree (event_id, is_announcement);
 CREATE INDEX posts_timestamp_index ON posts USING btree (timestamp);
-CREATE INDEX posts_timestamp_index ON posts USING btree (rating) WHERE is_announcement = false;
+CREATE INDEX posts_rating_index ON posts USING btree (rating) WHERE is_announcement = false;
 
 CREATE INDEX comments_index ON comments USING hash (post_id);
 CREATE INDEX comments_timestamp_index ON comments USING btree (timestamp);
 
-CREATE INDEX tickets_index ON tickets USING hash (user_id, event_id);
+CREATE INDEX tickets_index ON tickets USING btree (user_id, event_id);
 
 CREATE INDEX issues_timestamp_index ON issues USING btree(timestamp);
-CREATE INDEX issues_timestamp_index ON issues USING btree(is_solved);
+CREATE INDEX issues_solved_index ON issues USING btree(is_solved);
 
-CREATE INDEX notifications_index ON notifications USING hash(user_id, is_dismissed);
-CREATE INDEX notifications_index ON notifications USING btree(timestamp);
+CREATE INDEX notifications_index ON notifications USING btree(user_id, is_dismissed);
+CREATE INDEX notifications_timestamp_index ON notifications USING btree(timestamp);
 
 --CREATE INDEX events_search_index ON events USING GIN (search);
 --CREATE INDEX users_search_index ON users USING GIN (search);
