09d1a140d393782b1171fbaf579e5e481d700f80
miguelpduarte <miguelpduarte98@gmail.com>
2019-04-20 04:51:35 +0100

    Renamed template files to clarify what is being used and what is not

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/cards';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 
-Route::get('/', function () {
-    return redirect('login');
-});
+// Route::get('/', function () {
+//     return redirect('login');
+// });
 
 
 // Cards
