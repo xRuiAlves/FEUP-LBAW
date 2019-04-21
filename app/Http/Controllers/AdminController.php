39dfaaa249a31850521d701b893f85d967915dff
xRuiAlves <up201606746@fe.up.pt>
2019-04-21 10:28:29 +0100

    Added admin verification on admin page access

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Event;
 use App\User;
 use App\Issue;
@@ -26,6 +27,9 @@ class AdminController extends Controller
      */
     public function users()
     {
+        if(!Auth::user()->is_admin) { // TODO: Change this to use policies
+            return abort(401, 'You do not possess the required permissions to acces the administration pages');
+        }
         return view('pages.admin.users', ['users' => User::paginate(10)]);
     }
 
@@ -36,6 +40,9 @@ class AdminController extends Controller
      */
     public function events()
     {
+        if(!Auth::user()->is_admin) { // TODO: Change this to use policies
+            return abort(401, 'You do not possess the required permissions to acces the administration pages');
+        }
         return view('pages.admin.events', ['events' => Event::paginate(10)]);
     }
 
@@ -46,6 +53,9 @@ class AdminController extends Controller
      */
     public function issues()
     {
+        if(!Auth::user()->is_admin) { // TODO: Change this to use policies
+            return abort(401, 'You do not possess the required permissions to acces the administration pages');
+        }
         return view('pages.admin.issues', ['issues' => Issue::paginate(10)]);
     }
 }
