fb6c21e694349b2fbc43763ef17207e8e2bbeac9
up201606516 <up201606516@fe.up.pt>
2019-06-04 17:17:58 +0100

    Removed console.logs

--- public/js/active_events.js
+++ public/js/active_events.js
@@ -7,10 +7,8 @@ const addFavoriteClickEvent = () => {
         const event_id = favorite_btn.getAttribute('data-event-id');
         
         if(new_val) { // favorited event - must query to mark favorite
-            console.log('marked as favorite');
             sendFavoriteRequest('/api/event/favorite', 'POST', {event_id})
             .then(res => {
-                console.log(res);
                 
                 if (res.status === 200) {
                     res.json()
@@ -18,34 +16,17 @@ const addFavoriteClickEvent = () => {
                         favorite_btn.classList.add('active');
                         
                     });
-                } else {
-                    res.json()
-                    .then(json => {
-                        console.log('====================================');
-                        console.dir("dont toggle the button here instead of bootstrap toggle");
-                        console.log('====================================');
-                    });
                 }
             });
         } else { //unfavorited -- remove favorite
-            console.log('unmarked as favorite');
-
 
             sendFavoriteRequest('/api/event/favorite', 'DELETE', {event_id})
             .then(res => {
-                console.log(res);
                 
                 if (res.status === 200) {
                     res.json()
                     .then(json => {
                         favorite_btn.classList.remove('active');
-                    });
-                } else {
-                    res.json()
-                    .then(json => {
-                        console.log('====================================');
-                        console.dir("dont toggle the button here instead of bootstrap toggle");
-                        console.log('====================================');
                     });
                 }
             });
