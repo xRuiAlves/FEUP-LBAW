4c3b4150f846d7b300006d2ad0efeab52e3b1193
xRuiAlves <up201606746@fe.up.pt>
2019-06-05 22:27:40 +0100

    Disabled users now can't login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'These credentials do not match our records. Your account may have been disabled.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
